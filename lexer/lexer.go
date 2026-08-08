c986611da7ee4e17e4d57ef911b2e3fdfce70822
Puggy Potato <qingyaurou@gmail.com>
2026-08-08 22:03:41 +0800

    feat: add Lexer struct and New initialization

---
+++ lexer/lexer.go
@@ -0,0 +1,24 @@
+package lexer
+
+type Lexer struct {
+	input string // The input string
+	position int // Current Position 
+	readPosition int // Next Position 
+	ch byte // Current character 
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
