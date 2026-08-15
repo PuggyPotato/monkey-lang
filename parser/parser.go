7305f8dc00b9596ca9c980dde4978a5469929784
Puggy Potato <qingyaurou@gmail.com>
2026-08-16 01:33:45 +0800

    Add generic testInfixExpression helper
    Updates the testing suite to use interface{} so a single helper function can test infix expressions with both integers (5 + 5) and identifiers (a * b). Adds the table-driven test for parsing infix expressions.

--- parser/parser.go
+++ parser/parser.go
@@ -195,7 +195,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	
 	leftExp := prefix()
 
-	for !p.peekTokenIs(token.SEMICOLON) && precedence <p.peekPrecedence() {
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
 		infix := p.infixParseFns[p.peekToken.Type]
 		if infix == nil {
 			return leftExp
@@ -273,3 +273,5 @@ func (p *Parser) parseInfixExpression (left ast.Expression) ast.Expression {
 
 	return expression
 }
+
+
