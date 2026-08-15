f86afc61cae172880e6cb609b84dd1e49d19fa13
Puggy Potato <qingyaurou@gmail.com>
2026-08-15 17:37:04 +0800

    Add operator precedence tests and fix InfixExpression formatting
    Verifies that the Pratt parser correctly handles complex operator precedence and left-associativity. Fixes a bug in the AST where InfixExpressions were missing their closing parentheses during string serialization.

--- ast/ast.go
+++ ast/ast.go
@@ -154,6 +154,7 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(ie.Left.String())
 	out.WriteString(" " + ie.Operator + " ")
 	out.WriteString(ie.Right.String())
+	out.WriteString(")")
 
 	return out.String()
 }
