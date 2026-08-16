3d8274999b03b5b865b283339595e017de27c9b4
Puggy Potato <qingyaurou@gmail.com>
2026-08-16 22:29:04 +0800

    feat: complete parsing of let statements
    - Removed temporary token skipping hack in parseLetStatement
    
    - Integrated expression parsing for the right side of the '=' assignment
    
    - Refactored TestLetStatements to use a table-driven approach
    
    - Added assertions to verify integer, boolean, and identifier assignments

--- ast/ast.go
+++ ast/ast.go
@@ -82,6 +82,10 @@ func (ls *LetStatement) String() string {
 	out.WriteString(ls.TokenLiteral() + " ")
 	out.WriteString(ls.Name.String())
 	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
 	out.WriteString(";")
 
 	return out.String()
--- parser/parser.go
+++ parser/parser.go
@@ -128,8 +128,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: Skipping the expression until we encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -168,8 +171,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: Skipping the expressions until we encounter a semicolon.
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
