faf23c99d786b3d3af2af8ba141fd3f58398941f
Puggy Potato <qingyaurou@gmail.com>
2026-08-16 22:14:25 +0800

    feat: add parsing for function calls
    - Added CallExpression AST node for function invocations
    
    - Registered infix parse function for the LPAREN token to handle calls
    
    - Implemented parseCallArguments to parse comma-separated arguments
    
    - Assigned highest precedence (CALL) to the LPAREN token
    
    - Added TestCallExpressionParsing to verify function identifiers and arguments

--- parser/parser.go
+++ parser/parser.go
@@ -6,7 +6,6 @@ import (
 	"monkey/lexer"
 	"monkey/token"
 	"strconv"
-
 )
 
 type Parser struct {
@@ -45,6 +44,7 @@ var precedence = map[token.TokenType]int {
 	token.MINUS: SUM,
 	token.SLASH: PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN: CALL,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -70,6 +70,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
 	p.registerInfix(token.SLASH, p.parseInfixExpression)
@@ -393,4 +394,34 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 	}
 
 	return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{ Token: p.curToken, Function: function }
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+	
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil 
+	}
+
+	return args
 }
