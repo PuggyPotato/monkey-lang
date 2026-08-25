fd6e6cde695c9f4b9c999fbbba199ddcf76d0e00
Puggy Potato <qingyaurou@gmail.com>
2026-08-25 21:29:25 +0800

    Update token structure to include colon

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -67,6 +67,8 @@ func (l *Lexer) NextToken() token.Token {
 			tok = newToken(token.GT, l.ch)
 		case ',':
 			tok = newToken(token.COMMA, l.ch)
+		case ':':
+			tok = newToken(token.COLON, l.ch)
 		case '"':
 			tok.Type = token.STRING
 			tok.Literal = l.readString()
