9e20a6b1099cf10251845f2dcba6ccae5d714eea
Puggy Potato <qingyaurou@gmail.com>
2026-08-25 20:07:37 +0800

    feat: implement ArrayLiteral parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -752,3 +752,30 @@ func TestStringLiteralExpression(t *testing.T) {
 		t.Errorf("literal value not %q. got=%q.", "hello world", literal.Value)
 	}
 }
+
+func TestParsingArrayLiteral(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement, got=%T", program.Statements[0])
+	}
+
+	array, ok := stmt.Expression.(*ast.ArrayLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.ArrayLiteral, got=%T", stmt.Expression)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("len(array.Elements) not 3. got=%d", len(array.Elements))
+	}
+
+	testIntegerLiteral(t, array.Elements[0], 1)
+	testInfixExpression(t, array.Elements[1], 2, "*", 2)
+	testInfixExpression(t, array.Elements[2], 3, "+", 3)
+}
